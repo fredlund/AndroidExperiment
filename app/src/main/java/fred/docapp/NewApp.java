dfcaa1d2bc8724ee1785a6dd6e292f713b14f53c
Lars-Ake Fredlund <lfredlund@fi.upm.es>
2015-10-29 16:06:00 +0100

    sdffddf

--- app/src/main/java/fred/docapp/NewApp.java
+++ app/src/main/java/fred/docapp/NewApp.java
@@ -1,9 +1,12 @@
 package fred.docapp;
 
 import android.app.SearchManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.SearchView;
@@ -24,13 +27,14 @@ public class NewApp extends AppCompatActivity {
 
         // Get the SearchView and set the searchable configuration
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-       System.out.println("item is "+menu.findItem(R.id.menu_search));
+        System.out.println("item is "+menu.findItem(R.id.menu_search));
         SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-        System.out.println("searchView="+searchView);
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+        System.out.println("searchView=" + searchView);
+        System.out.flush();
+        ComponentName cn = new ComponentName(this, SearchableActivity.class);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(cn));
+       searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
 
-   return true;
+      return true;
     }
 }
--- app/src/main/java/fred/docapp/SearchableActivity.java
+++ app/src/main/java/fred/docapp/SearchableActivity.java
@@ -1,26 +1,71 @@
 package fred.docapp;
 
+import android.app.ListActivity;
 import android.app.SearchManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-public class SearchableActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SearchableActivity extends ListActivity { //AppCompatActivity {
+
+    private List<Map<String,Object>> listValues;
+    private ListView listView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("search activitity was created");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search);
+        listValues = new ArrayList<Map<String,Object>>();
+        prepareData(listValues);
+        setContentView(R.layout.mylist);
+        listView = (ListView) findViewById(android.R.id.list);
+
+    SimpleAdapter adapter = new SimpleAdapter(this, listValues,android.R.layout.simple_list_item_1,new String[] { "1" },new int[] { android.R.id.text1 });
+    listView.setAdapter(adapter);
 
         // Get the intent, verify the action and get the query
+        System.out.println("before intent");
+        System.out.flush();
         Intent intent = getIntent();
+        System.out.println("after intent");
+        System.out.flush();
+        handleIntent(intent);
+    }
+
+    void prepareData(List<Map<String,Object>> listValues) {
+        Map<String,Object> item;
+        item = new HashMap<String,Object>();
+        item.put("1","A");
+        listValues.add(item);
+        item = new HashMap<String,Object>();
+        item.put("1","B");
+        listValues.add(item);
+    }
+
+    //@Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+ private void handleIntent(Intent intent) {
+        System.out.println("handleIntent "+intent);
+        System.out.flush();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
             doMySearch(query);
+            //finish();
         }
     }
 
     void doMySearch(String query) {
-
+      System.out.println("will search for "+query);
+        System.out.flush();
     }
 }
