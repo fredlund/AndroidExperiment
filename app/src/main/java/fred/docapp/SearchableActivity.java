6d9c6f8a16d717961fcafb5cbb73ce7ffeacf3b1
Lars-Ake Fredlund <lfredlund@fi.upm.es>
2015-11-08 12:22:25 +0100

    Progress dialog.

--- app/src/main/java/fred/docapp/SearchableActivity.java
+++ app/src/main/java/fred/docapp/SearchableActivity.java
@@ -1,9 +1,11 @@
 package fred.docapp;
 
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.v4.view.MenuItemCompat;
@@ -181,13 +183,27 @@ public class SearchableActivity extends AppCompatActivity {
         }
     }
 
-    void doMySearch(String query,List<Map<String,Object>> listValues) {
+    void doMySearch(final String query,List<Map<String,Object>> listValues) {
       System.out.println("will search for " + query);
         System.out.flush();
         final MLocate mloc = new MLocate();
-        found = mloc.find(query,SearchableActivity.this);
-        System.out.println("size of found: " + found.length);
-        System.out.flush();
+        final ProgressDialog pd = new ProgressDialog(SearchableActivity.this);
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+
+		@Override
+		protected void onPreExecute() {
+			pd.setTitle("Processing...");
+			pd.setMessage("Please wait.");
+			pd.setCancelable(false);
+			pd.setIndeterminate(true);
+			pd.show();
+		}
+
+		@Override
+		protected Void doInBackground(Void... arg0) {
+            found = mloc.find(query,SearchableActivity.this);
+            System.out.println("size of found: " + found.length);
+            System.out.flush();
 
         /*for (Entry entry : found) {
             String value;
@@ -200,94 +216,111 @@ public class SearchableActivity extends AppCompatActivity {
             listValues.add(item);
         }*/
 
+            return null;
+		}
 
-        EntryAdapter adapter = new EntryAdapter(this,
-                R.layout.listview_item_row, found);
+		@Override
+		protected void onPostExecute(Void result) {
+			if (pd!=null) {
+				pd.dismiss();
 
-        System.out.println("before stack push");
-        System.out.flush();
+			}
+            EntryAdapter adapter = new EntryAdapter(SearchableActivity.this,
+                    R.layout.listview_item_row, found);
+
+            System.out.println("before stack push");
+            System.out.flush();
             stack = new ArrayList<DirView>();
-        //DirView dv = new DirView(name(mloc.root),adapter,found);
-        //stack.push(dv);
-        System.out.println("after stack push");
-        System.out.flush();
+            //DirView dv = new DirView(name(mloc.root),adapter,found);
+            //stack.push(dv);
+            System.out.println("after stack push");
+            System.out.flush();
 
-        setContentView(R.layout.mylist);
-         listView1 = (ListView)findViewById(android.R.id.list);
-         listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            setContentView(R.layout.mylist);
+            listView1 = (ListView)findViewById(android.R.id.list);
+            listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-             @Override
-             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 System.out.println("onItemClick position="+position+" found size="+
-                         ((found==null)?-1:found.length));
-                 System.out.flush();
-                 Entry entry = found[position];
-                 System.out.println("item " + position + " was clicked=" + found[position]);
-                 if (entry.entryType != Entry.EntryType.File) {
-                     System.out.println("will try to read " + entry);
-                     Entry[] dirEntries = mloc.read_dir(SearchableActivity.this, entry);
-                     if (dirEntries != null && dirEntries.length > 0) {
-                         found = dirEntries;
-                         System.out.println("numer of entries are :" + dirEntries.length);
-                         System.out.flush();
-                         EntryAdapter adapter = new EntryAdapter(myself, R.layout.listview_item_row, dirEntries);
-                         System.out.println("computed new adapter");
-                         System.out.flush();
-                         listView1.setAdapter(adapter);
-                         System.out.println("set adapter");
-                         System.out.flush();
-                         DirView dv = new DirView(entry.fileName, adapter, dirEntries);
-                         stack.add(dv);
-                         System.out.println("pushed");
-                         System.out.flush();
-                         System.out.println("before spinner adapter");
-                         System.out.flush();
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    System.out.println("onItemClick position="+position+" found size="+
+                            ((found==null)?-1:found.length));
+                    System.out.flush();
+                    Entry entry = found[position];
+                    System.out.println("item " + position + " was clicked=" + found[position]);
+                    if (entry.entryType != Entry.EntryType.File) {
+                        System.out.println("will try to read " + entry);
+                        Entry[] dirEntries = mloc.read_dir(SearchableActivity.this, entry);
+                        if (dirEntries != null && dirEntries.length > 0) {
+                            found = dirEntries;
+                            System.out.println("numer of entries are :" + dirEntries.length);
+                            System.out.flush();
+                            EntryAdapter adapter = new EntryAdapter(myself, R.layout.listview_item_row, dirEntries);
+                            System.out.println("computed new adapter");
+                            System.out.flush();
+                            listView1.setAdapter(adapter);
+                            System.out.println("set adapter");
+                            System.out.flush();
+                            DirView dv = new DirView(entry.fileName, adapter, dirEntries);
+                            stack.add(dv);
+                            System.out.println("pushed");
+                            System.out.flush();
+                            System.out.println("before spinner adapter");
+                            System.out.flush();
 
-                         spinnerAdapter = new OurSpinnerAdapter(SearchableActivity.this,
-                                 android.R.layout.simple_spinner_item,
-                                 //R.layout.spinner_item_row,
-                                 //R.id.spinnerText,
-                                 stack);
+                            spinnerAdapter = new OurSpinnerAdapter(SearchableActivity.this,
+                                    android.R.layout.simple_spinner_item,
+                                    //R.layout.spinner_item_row,
+                                    //R.id.spinnerText,
+                                    stack);
 
-                         System.out.println("after spinner adapter");
-                         System.out.flush();
-                          spinner.setAdapter(spinnerAdapter);
-                     }
-                 }
-             }
+                            System.out.println("after spinner adapter");
+                            System.out.flush();
+                            spinner.setAdapter(spinnerAdapter);
+                        }
+                    }
+                }
 
-         });
+            });
 
-        listView1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            listView1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Entry entry = found[position];
-                System.out.println("item " + position + " was longclicked=" + entry);
-                entry.isEnabled = !entry.isEnabled;
-                EntryAdapter.EntryHolder holder = (EntryAdapter.EntryHolder) view.getTag();
-                if (entry.isEnabled)
-                    holder.image.setImageResource(R.drawable.ic_done_white);
-                else
-                    holder.image.setImageBitmap(null);
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                    Entry entry = found[position];
+                    System.out.println("item " + position + " was longclicked=" + entry);
+                    entry.isEnabled = !entry.isEnabled;
+                    EntryAdapter.EntryHolder holder = (EntryAdapter.EntryHolder) view.getTag();
+                    if (entry.isEnabled)
+                        holder.image.setImageResource(R.drawable.ic_done_white);
+                    else
+                        holder.image.setImageBitmap(null);
 
-                return true;
-            }
+                    return true;
+                }
 
-        });
-        listView1.setAdapter(adapter);
+            });
+            listView1.setAdapter(adapter);
 
-        System.out.println("before spinner adapter");
-        System.out.flush();
+            System.out.println("before spinner adapter");
+            System.out.flush();
 
-        spinnerAdapter = new OurSpinnerAdapter(SearchableActivity.this,
-                 android.R.layout.simple_spinner_item,
-                //R.id.spinnerText,
-                //R.layout.spinner_item_row,
-                stack);
+            spinnerAdapter = new OurSpinnerAdapter(SearchableActivity.this,
+                    android.R.layout.simple_spinner_item,
+                    //R.id.spinnerText,
+                    //R.layout.spinner_item_row,
+                    stack);
 
-        System.out.println("after spinner adapter");
-        System.out.flush();
+            System.out.println("after spinner adapter");
+            System.out.flush();
+		}
+
+	};
+	task.execute((Void[]) null);
+
+
+
+
+
     }
 
     String name(String path) {
