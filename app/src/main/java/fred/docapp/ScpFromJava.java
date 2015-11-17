c3d3ea4164f28ca760bf72539ce4eb5f71e511d6
Lars-Ake Fredlund <lfredlund@fi.upm.es>
2015-11-17 01:33:40 +0100

    Fix

--- app/src/main/java/fred/docapp/ScpFromJava.java
+++ app/src/main/java/fred/docapp/ScpFromJava.java
@@ -8,6 +8,101 @@ import com.jcraft.jsch.*;
 import java.io.*;
 
 public class ScpFromJava {
+    public boolean transfer(String username, String password, String host, String file) {
+        JSch jsch=new JSch();
+        Session session=jsch.getSession(user, host, 22);
+
+         try{
+
+        session.connect();
+
+        // exec 'scp -f rfile' remotely
+        String command="scp -f "+file;
+        Channel channel=session.openChannel("exec");
+        ((ChannelExec)channel).setCommand(command);
+
+        // get I/O streams for remote scp
+        OutputStream out=channel.getOutputStream();
+        InputStream in=channel.getInputStream();
+
+        channel.connect();
+
+        byte[] buf=new byte[1024];
+
+        // send '\0'
+        buf[0]=0; out.write(buf, 0, 1); out.flush();
+
+        while(true){
+            int c=checkAck(in);
+            if(c!='C'){
+                break;
+            }
+
+            // read '0644 '
+            in.read(buf, 0, 5);
+
+            long filesize=0L;
+            while(true){
+                if(in.read(buf, 0, 1)<0){
+                    // error
+                    break;
+                }
+                if(buf[0]==' ')break;
+                filesize=filesize*10L+(long)(buf[0]-'0');
+            }
+
+            String file=null;
+            for(int i=0;;i++){
+                in.read(buf, i, 1);
+                if(buf[i]==(byte)0x0a){
+                    file=new String(buf, 0, i);
+                    break;
+                }
+            }
+
+            //System.out.println("filesize="+filesize+", file="+file);
+
+            // send '\0'
+            buf[0]=0; out.write(buf, 0, 1); out.flush();
+
+            // read a content of lfile
+            FileOutputStream fos = context.openFileInput(file);
+
+            fos=new FileOutputStream(prefix==null ? lfile : prefix+file);
+            int foo;
+            while(true){
+                if(buf.length<filesize) foo=buf.length;
+                else foo=(int)filesize;
+                foo=in.read(buf, 0, foo);
+                if(foo<0){
+                    // error
+                    break;
+                }
+                fos.write(buf, 0, foo);
+                filesize-=foo;
+                if(filesize==0L) break;
+            }
+            fos.close();
+            fos=null;
+
+            if(checkAck(in)!=0){
+                System.exit(0);
+            }
+
+            // send '\0'
+            buf[0]=0; out.write(buf, 0, 1); out.flush();
+        }
+
+        session.disconnect();
+
+        System.exit(0);
+    }
+    catch(Exception e){
+        System.out.println(e);
+        try{if(fos!=null)fos.close();}catch(Exception ee){}
+    }
+    }
+
     public static void main(String[] arg){
         if(arg.length!=2){
             System.err.println("usage: java ScpFrom user@remotehost:file1 file2");
@@ -15,7 +110,6 @@ public class ScpFromJava {
         }
 
         FileOutputStream fos=null;
-        try{
 
             String user=arg[0].substring(0, arg[0].indexOf('@'));
             arg[0]=arg[0].substring(arg[0].indexOf('@')+1);
@@ -28,94 +122,7 @@ public class ScpFromJava {
                 prefix=lfile+File.separator;
             }
 
-            JSch jsch=new JSch();
-            Session session=jsch.getSession(user, host, 22);
 
-            session.connect();
-
-            // exec 'scp -f rfile' remotely
-            String command="scp -f "+rfile;
-            Channel channel=session.openChannel("exec");
-            ((ChannelExec)channel).setCommand(command);
-
-            // get I/O streams for remote scp
-            OutputStream out=channel.getOutputStream();
-            InputStream in=channel.getInputStream();
-
-            channel.connect();
-
-            byte[] buf=new byte[1024];
-
-            // send '\0'
-            buf[0]=0; out.write(buf, 0, 1); out.flush();
-
-            while(true){
-                int c=checkAck(in);
-                if(c!='C'){
-                    break;
-                }
-
-                // read '0644 '
-                in.read(buf, 0, 5);
-
-                long filesize=0L;
-                while(true){
-                    if(in.read(buf, 0, 1)<0){
-                        // error
-                        break;
-                    }
-                    if(buf[0]==' ')break;
-                    filesize=filesize*10L+(long)(buf[0]-'0');
-                }
-
-                String file=null;
-                for(int i=0;;i++){
-                    in.read(buf, i, 1);
-                    if(buf[i]==(byte)0x0a){
-                        file=new String(buf, 0, i);
-                        break;
-                    }
-                }
-
-                //System.out.println("filesize="+filesize+", file="+file);
-
-                // send '\0'
-                buf[0]=0; out.write(buf, 0, 1); out.flush();
-
-                // read a content of lfile
-                fos=new FileOutputStream(prefix==null ? lfile : prefix+file);
-                int foo;
-                while(true){
-                    if(buf.length<filesize) foo=buf.length;
-                    else foo=(int)filesize;
-                    foo=in.read(buf, 0, foo);
-                    if(foo<0){
-                        // error
-                        break;
-                    }
-                    fos.write(buf, 0, foo);
-                    filesize-=foo;
-                    if(filesize==0L) break;
-                }
-                fos.close();
-                fos=null;
-
-                if(checkAck(in)!=0){
-                    System.exit(0);
-                }
-
-                // send '\0'
-                buf[0]=0; out.write(buf, 0, 1); out.flush();
-            }
-
-            session.disconnect();
-
-            System.exit(0);
-        }
-        catch(Exception e){
-            System.out.println(e);
-            try{if(fos!=null)fos.close();}catch(Exception ee){}
-        }
     }
 
 
