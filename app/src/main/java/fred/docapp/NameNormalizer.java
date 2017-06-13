e6584f885464ea57fd15342a1ef7c2c3df0c4295
Lars-Ake Fredlund <lfredlund@fi.upm.es>
2017-06-13 15:44:22 +0200

    Sorting support

--- app/src/main/java/fred/docapp/NameNormalizer.java
+++ app/src/main/java/fred/docapp/NameNormalizer.java
@@ -7,32 +7,55 @@ package fred.docapp;
 public class NameNormalizer {
     public static String normalizeFileName(String fileName) {
         // First strip characters that do not interest us, then remove path, then strip character combinations
-        return removeStrings(fileName,"cbr","cbz","rar","epub","pdf","zip","rtf","jpg","bdfr","bd","crg");
+        return removeStrings(fileName,"bd fr","bdfr","bd","crg");
     }
 
     static String removeStrings(String origString, String... removes) {
         boolean changed;
 
-        StringBuilder builder = new StringBuilder(origString.toLowerCase());
-
-        for (int i=builder.length()-1; i>=0; i--) {
-            int ch = (int) builder.charAt(i);
-            if ((ch >= 48 && ch <= 57) || (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
-                continue;
-            else
-                builder.deleteCharAt(i);
-        }
-        origString = builder.toString();
+        String strippedString = stripSymbols(origString);
+        System.out.println("origString="+origString+" strippedString="+strippedString);
         do {
             changed = false;
             for (String removeString : removes) {
-                if (origString.contains(removeString)) {
-                    origString = origString.replace(removeString,"");
+                if (strippedString.contains(removeString)) {
+                    strippedString = strippedString.replace(removeString,"");
                     changed = true;
                     break;
                 }
             }
         } while (changed);
-        return origString;
+        String finalString = stripSymbols(strippedString);
+        System.out.println("cutString="+strippedString+" final="+finalString);
+        return finalString;
+    }
+
+    static String stripSymbols(String origString) {
+        StringBuilder builder = new StringBuilder(origString.toLowerCase());
+        int lastChar = -1;
+        boolean skippedSymbols = false;
+        boolean outputSymbol = false;
+
+        int i = 0;
+        while (i < builder.length()) {
+            int ch = (int) builder.charAt(i);
+            if (ch == 48 && (!outputSymbol || skippedSymbols))
+                builder.deleteCharAt(i);
+            else if (!isLetterOrDigit(ch)) {
+                skippedSymbols = true;
+                builder.deleteCharAt(i);
+            } else {
+                if (skippedSymbols && outputSymbol) builder.insert(i,' ');
+                skippedSymbols = false;
+                outputSymbol = true;
+                i++;
+            }
+            lastChar = ch;
+        }
+        return builder.toString();
+    }
+
+    static boolean isLetterOrDigit(int ch) {
+        return (ch >= 48 && ch <= 57) || (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122);
     }
 }
